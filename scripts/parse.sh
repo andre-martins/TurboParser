126d97d87132d8cc092c159ea5fcc2c5abc5a3ff
Andre Martins <andre.t.martins@gmail.com>
2012-09-25 01:57:46 +0100

    Added tokenizer.sed to the scripts and fixed a few things in the distribution.

--- scripts/parse.sh
+++ scripts/parse.sh
@@ -11,7 +11,7 @@ fi
 
 cd ${DIR}
 
-${DIR}/create_conll_corpus_from_text.sh ${tmp_folder}/tmp.tokenized > ${tmp_folder}/tmp.conll
+${DIR}/create_conll_corpus_from_text.pl ${tmp_folder}/tmp.tokenized > ${tmp_folder}/tmp.conll
 ${DIR}/create_tagging_corpus.sh ${tmp_folder}/tmp.conll # Creates tmp.conll.tagging.
 ${DIR}/run_tagger.sh ${tmp_folder}/tmp.conll.tagging # Creates tmp.conll.tagging.pred.
 ${DIR}/create_conll_predicted_tags_corpus.sh ${tmp_folder}/tmp.conll ${tmp_folder}/tmp.conll.tagging.pred # Creates tmp.conll.predpos
