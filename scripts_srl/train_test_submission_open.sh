2938248da5dc00856eb202de6b56715843e8ffe8
Andre Martins <andre.t.martins@gmail.com>
2014-07-13 17:01:25 +0100

    FIX Uncommented some runs in the submission script.

--- scripts_srl/train_test_submission_open.sh
+++ scripts_srl/train_test_submission_open.sh
@@ -1,19 +1,19 @@
-model_type=af #af+as+cs+gp+cp+ccp
-open=true #false
+model_type=af+as+cs+gp+cp+ccp
+open=true
 
 formalism=dm
 C=0.01
 cost_fp=0.4
 cost_fn=0.6
 echo "${C} ${cost_fp} ${cost_fn} ${formalism} ${model_type} ${open}"
-#./train_test_semantic_parser.sh english ${C} ${cost_fp} ${cost_fn} ${model_type} ${open} ${formalism} >& out_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
+./train_test_semantic_parser.sh english ${C} ${cost_fp} ${cost_fn} ${model_type} ${open} ${formalism} >& out_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
 
 formalism=pas
 C=0.01
 cost_fp=0.4
 cost_fn=0.6
 echo "${C} ${cost_fp} ${cost_fn} ${formalism} ${model_type} ${open}"
-#./train_test_semantic_parser.sh english ${C} ${cost_fp} ${cost_fn} ${model_type} ${open} ${formalism} >& out_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
+./train_test_semantic_parser.sh english ${C} ${cost_fp} ${cost_fn} ${model_type} ${open} ${formalism} >& out_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
 
 formalism=pcedt
 C=0.01
