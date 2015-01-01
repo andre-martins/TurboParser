aaf49938338b0b8a19540d33c77fdf4631e4105c
Andre Martins <andre.t.martins@gmail.com>
2015-01-01 10:20:15 +0000

    ENH New script for SDP 2015.

--- scripts_srl/train_test_submission_SDP2015.sh
+++ scripts_srl/train_test_submission_SDP2015.sh
@@ -1,13 +1,21 @@
 model_type=basic #af+as+cs+gp+cp+ccp
 open=true
 
+language=chinese
+formalism=pas
+C=0.01
+cost_fp=0.4
+cost_fn=0.6
+echo "${language} ${C} ${cost_fp} ${cost_fn} ${formalism} ${model_type} ${open}"
+./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} sdp ${formalism} ${open} >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
+
 language=english
 formalism=dm
 C=0.01
 cost_fp=0.4
 cost_fn=0.6
 echo "${language} ${C} ${cost_fp} ${cost_fn} ${formalism} ${model_type} ${open}"
-./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} ${open} ${formalism} sdp >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
+./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} sdp ${formalism} ${open} >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
 
 language=english
 formalism=pas
@@ -15,7 +23,7 @@ C=0.01
 cost_fp=0.4
 cost_fn=0.6
 echo "${language} ${C} ${cost_fp} ${cost_fn} ${formalism} ${model_type} ${open}"
-./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} ${open} ${formalism} sdp >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
+./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} sdp ${formalism} ${open} >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
 
 language=english
 formalism=psd
@@ -23,15 +31,7 @@ C=0.01
 cost_fp=0.4
 cost_fn=0.6
 echo "${language} ${C} ${cost_fp} ${cost_fn} ${formalism} ${model_type} ${open}"
-./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} ${open} ${formalism} sdp >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
-
-language=chinese
-formalism=pas
-C=0.01
-cost_fp=0.4
-cost_fn=0.6
-echo "${language} ${C} ${cost_fp} ${cost_fn} ${formalism} ${model_type} ${open}"
-./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} ${open} ${formalism} sdp >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
+./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} sdp ${formalism} ${open} >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
 
 language=czech
 formalism=psd
@@ -39,4 +39,4 @@ C=0.01
 cost_fp=0.4
 cost_fn=0.6
 echo "${language} ${C} ${cost_fp} ${cost_fn} ${formalism} ${model_type} ${open}"
-./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} ${open} ${formalism} sdp >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
+#./train_test_semantic_parser.sh ${language} ${C} ${cost_fp} ${cost_fn} ${model_type} sdp ${formalism} ${open} >& out_SDP2015_${language}_open-${open}_deterministic_${formalism}_${C}_${cost_fp}_${cost_fn}_${model_type}
