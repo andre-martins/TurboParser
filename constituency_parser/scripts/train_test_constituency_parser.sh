add840acaee3df6fced1f79d4f299a1cb1849fce
André T. Martins <atm@hyperv-ubuntu2.(none)>
2015-01-20 11:14:11 +0000

    FIX Script for constituency parsing.

--- constituency_parser/scripts/train_test_constituency_parser.sh
+++ constituency_parser/scripts/train_test_constituency_parser.sh
@@ -8,7 +8,7 @@ language=$1 # Example: "english_ptb".
 C_labeler=0.01 #0.01
 C_parser=0.001
 C_unary_predictor=1.0
-delta_encoding=false
+delta_encoding=true #false
 parser_model_type=full
 suffix_parser=parser_pruned-true_model-${parser_model_type}.pred
 
@@ -29,32 +29,33 @@ then
     files_test_conll[1]=${path_data}/${language}_ftags_dev.conll.predpos
 fi
 
+./train_test_parser.sh ${language} ${C_parser} ${parser_model_type}
+
 if true
 then
-# Convert constituency trees to dependency trees using an existing dependency
-# file as a guide.
-file_train_trees_conll=${file_train_trees}.conll
-rm -f file_train_trees_conll
-java -jar -Dfile.encoding=utf-8 converter.jar conv-auto \
-    ${file_train_trees} \
-    ${file_train_trees_conll} \
-    true \
-    ${file_train_conll}
-for (( i=0; i<${#files_test_trees[*]}; i++ ))
-do
-    file_test_trees=${files_test_trees[$i]}
-    file_test_conll=${files_test_conll[$i]}
-    file_test_trees_conll=${file_test_trees}.conll
-    rm -f file_test_trees_conll
+    # Convert constituency trees to dependency trees using an existing dependency
+    # file as a guide.
+    file_train_trees_conll=${file_train_trees}.conll
+    rm -f file_train_trees_conll
     java -jar -Dfile.encoding=utf-8 converter.jar conv-auto \
-        ${file_test_trees} \
-        ${file_test_trees_conll} \
-        true \
-        ${file_test_conll}
-done
+	${file_train_trees} \
+	${file_train_trees_conll} \
+	true \
+	${file_train_conll}
+    for (( i=0; i<${#files_test_trees[*]}; i++ ))
+    do
+	file_test_trees=${files_test_trees[$i]}
+	file_test_conll=${files_test_conll[$i]}
+	file_test_trees_conll=${file_test_trees}.conll
+	rm -f file_test_trees_conll
+	java -jar -Dfile.encoding=utf-8 converter.jar conv-auto \
+            ${file_test_trees} \
+            ${file_test_trees_conll} \
+            true \
+            ${file_test_conll}
+    done
 fi
 
-#./train_test_parser.sh ${language} ${C_parser} ${parser_model_type}
 ./train_test_dependency_labeler.sh ${language} ${C_labeler} ${delta_encoding}
 ./train_test_constituency_labeler.sh ${language} ${C_unary_predictor} 0.5 0.5 true
 
