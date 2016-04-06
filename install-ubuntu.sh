22eb6f536a033813922026503112685a707a7e48
Pedro Balage <pedrobalage@gmail.com>
2016-04-06 11:54:35 +0100

    Created an automatic Ubuntu install script

---
+++ install-ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash                                                                                                                                                                                                        
+sudo apt-get install -y build-essential automake autoconf
+
+./install_deps.sh
+rm missing
+aclocal
+autoconf
+automake --add-missing
+./configure && make && make install
+
+sudo rm -rf /opt/TurboParser
+sudo cp -r "$PWD" /opt/TurboParser
+sudo ln -s /opt/TurboParser/Turbo* /usr/local/bin/
+
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/TurboParser/deps/local/lib:"  >> ~/.bashrc
+source ~/.bashrc
+echo
+echo "Installation finished"
+echo
