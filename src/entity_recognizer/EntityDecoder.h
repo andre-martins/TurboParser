132269b0623bbf41c0fb6b3f1b92f7c203a7cac2
Mariana Almeida <mla@priberam.pt>
2017-01-02 15:57:26 +0000

    clear unused code in Entity.cpp/.h

--- src/entity_recognizer/EntityDecoder.h
+++ src/entity_recognizer/EntityDecoder.h
@@ -28,70 +28,12 @@ public:
   EntityDecoder(SequencePipe *pipe) : SequenceDecoder(pipe) {}; // EntityPipe SequencePipe??  
   virtual ~EntityDecoder() {};
 
-  virtual void Decode(Instance *instance, Parts *parts,
-              const vector<double> &scores,
-              vector<double> *predicted_output);
-
   virtual void DecodeCostAugmented(Instance *instance, Parts *parts,
                            const vector<double> &scores,
                            const vector<double> &gold_output,
                            vector<double> *predicted_output,
                            double *cost,
                            double *loss);
-
-  virtual void DecodeCostAugmentedMarginals(Instance *instance, Parts *parts,
-                                            const vector<double> &scores,
-                                            const vector<double> &gold_output,
-                                            vector<double> *predicted_output,
-                                            double *entropy,
-                                            double *cost,
-                                            double *loss) {
-    CHECK(false) << "Not implemented yet.";
-  }
-
-  virtual void DecodeMarginals(Instance *instance, Parts *parts,
-                               const vector<double> &scores,
-                               const vector<double> &gold_output,
-                               vector<double> *predicted_output,
-                               double *entropy,
-                               double *loss) {
-    // TODO: Implement forward-backward.
-    CHECK(false) << "Not implemented yet.";
-  }
-
-#if 0
-  void ConvertToFirstOrderModel(
-    const std::vector<SequenceDecoderNodeScores> &node_scores,
-    const std::vector<SequenceDecoderEdgeScores> &edge_scores,
-    const std::vector<SequenceDecoderEdgeScores> &triplet_scores,
-    std::vector<SequenceDecoderNodeScores> *transformed_node_scores,
-    std::vector<SequenceDecoderEdgeScores> *transformed_edge_scores);
-
-  void RecoverBestPath(const std::vector<int> &best_path,
-                       std::vector<int> *transformed_best_path);
-
-  double SolveMarkovZeroOrder(const std::vector<SequenceDecoderNodeScores> &node_scores,
-                              std::vector<int> *best_path);
-
-  double RunViterbi(const vector<vector<double> > &node_scores,
-                    const vector<vector<vector<double > > >
-                    &edge_scores,
-                    vector<int> *best_path);
-
-  double RunViterbi(const std::vector<SequenceDecoderNodeScores> &node_scores,
-                    const std::vector<SequenceDecoderEdgeScores> &edge_scores,
-                    std::vector<int> *best_path);
-
-#ifdef USE_CPLEX
-  double DecodeCPLEX(Instance *instance, Parts *parts,
-                     const vector<double> &scores,
-                     bool relax,
-                     vector<double> *predicted_output);
-#endif  //
-#endif  // #if 0
-
-protected:
-  //EntityPipe *pipe_; //SequencePipe *pipe_;
 };
 
-#endif /* SEQUENCEDECODER_H_ */
+#endif /* ENTITYDECODER_H_ */
