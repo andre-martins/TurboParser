a87b8e45694c18b826bb3c42e8344bd32928007d
Andre Martins <andre.t.martins@gmail.com>
2019-03-21 14:14:37 +0000

    Forced compilation of python wrapper.

--- python/install_wrapper.sh
+++ python/install_wrapper.sh
@@ -11,8 +11,9 @@ python_folder=${root_folder}/python
 
 # Make a static lib.
 cd $lib_folder
+make clean
 make
 
 # Now use cython to build a Python wrapper.
 cd $python_folder
-python setup.py build_ext
+python setup.py build_ext --force
